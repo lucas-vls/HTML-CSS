feed2a976ce271bfcf5448432052073314f5317c
lucas-vls <lucas1veloso@gmail.com>
2022-12-09 10:42:32 -0300

    1

--- EXERCICIOS/MODULO4/proj.tel_login/cadastro.js
+++
@@ -1,9 +0,0 @@
-function cadastrar(){
-    let senha = document.getElementById('sen')
-    let csen = document.getElementById('csen')
-    if(senha.value===csen.value){
-        alert('td certo')
-    }else{
-     alert('Senhas não são iguais!')
-    }
-}
