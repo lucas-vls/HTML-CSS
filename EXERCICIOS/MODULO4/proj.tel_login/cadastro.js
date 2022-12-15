bcb46437383fb3fd954dcb78c797d68bf846d045
lucas-vls <lucas1veloso@gmail.com>
2022-12-14 23:22:18 -0300

    n

--- EXERCICIOS/MODULO4/proj.tel_login/cadastro.js
+++ EXERCICIOS/MODULO4/proj.tel_login/cadastro.js
@@ -1,6 +1,18 @@
+document.getElementById('btn').disabled=true;
+//linha
 let formulario = document.getElementById('formulario')
 let nome = document.getElementById('nome')
 let email = document.getElementById('email')
 let celular = document.getElementById('cel')
 let senha = document.getElementById('sen')
 let csenha = document.getElementById('csen')
+//linha
+
+function alertasenha(){
+    if(senha.value===csenha.value){
+        return true
+    } else{
+        alert('Senhas não são iguais!')
+    }
+    
+}
